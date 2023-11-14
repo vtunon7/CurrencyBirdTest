3608435be9c38b8443e184b20fd707447c40cf1a
Vicente <vtunon@uc.cl>
2023-11-14 15:41:21 -0300

    feat(): CHANGE ondex to src directory

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello asdfasfdfddd!");
+  res.send("Hello asdfasdfkjbsadkjfdsajafsjhklafsfddd!");
 });
 
 const port = process.env.PORT || 4000;
